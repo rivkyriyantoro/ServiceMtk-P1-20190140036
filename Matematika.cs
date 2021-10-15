254f7ae3019a4da670e7da7d7b48f71c011d2a7f
RivkyRiyantoro <sirivkyriyantoro@gmail.com>
2021-10-15 20:34:10 +0700

    Add project files.

---
+++ Matematika.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceMtk_P1_20190140036
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Matematika : IMatematika
+    {
+        public int Bagi(int a, int b)
+        {
+            return a / b;
+        }
+        public int Kali(int a, int b)
+        {
+            return a * b;
+        }
+        public int Kurang(int a, int b)
+        {
+            return a - b;
+        }
+        public int Tambah(int a, int b)
+        {
+            return a + b;
+        }
+
+        public Koordinat TKoordinat(Koordinat a, Koordinat b)
+        {
+            Koordinat hasil = new Koordinat();
+            hasil.X = a.X + b.X;
+            hasil.Y = a.Y + b.Y;
+            return hasil;
+        }
+    }
+}
+
